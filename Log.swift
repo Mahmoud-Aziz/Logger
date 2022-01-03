8df3688f5a270c290c3eb51453e9af8507860e46
“Mahmoud <mahmoudmohammedaziz@gmail.com>
2022-01-03 14:56:13 +0200

    Initial commit

---
+++ Log.swift
@@ -0,0 +1,63 @@
+//
+//  Log.swift
+//  Logger
+//
+//  Created by Mahmoud Aziz on 03/01/2022.
+//
+
+import Foundation
+
+
+enum Log {
+    enum LogLevel {
+        case info
+        case warning
+        case error
+        
+        fileprivate var prefix: String {
+            switch self {
+            case .info: return "INFO ℹ️"
+            case .warning: return "WARNING ⚠️"
+            case .error: return "ERROR ❌"
+            }
+        }
+    }
+    
+    struct Context {
+        let file: String
+        let function: String
+        let line: Int
+        var description: String {
+            return "\((file as NSString).lastPathComponent):\(line) \(function)"
+        }
+    }
+    
+    static func info(str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .info, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+    static func warning(str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .warning, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+    static func error(str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .error, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+     private static func handleLog(level: LogLevel, str: String, shouldLogContext: Bool, context: Context) {
+        let logComponents = ["\(level.prefix)", str]
+        
+        var fullString = logComponents.joined(separator: " ")
+        if shouldLogContext {
+            fullString += " - \(context.description)"
+        }
+        
+        #if DEBUG
+        print(fullString)
+        #endif
+    
+    }
+}
---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Logger
+//
+//  Created by Mahmoud Aziz on 03/01/2022.
+//
+
+import UIKit
+import Logging
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
